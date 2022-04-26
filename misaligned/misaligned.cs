cc931f30308407b352f899ba42a3e4505c4e7122
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-04-26 22:51:03 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,16 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 
 namespace MisalignedSpace
 {
   class Misaligned
   {
-    private static List<string> majorColorList = new List<string>();
-    private static List<string> minorColorList = new List<string>();
+    private static List<string> _majorColorList = new List<string>();
+    private static List<string> _minorColorList = new List<string>();
+    private static List<int> _pairNumber = new List<int>();
 
-    static int printColorMap()
+    static int PrintColorMap()
     {
       string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
       string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
@@ -20,27 +20,34 @@ namespace MisalignedSpace
         for (j = 0; j < 5; j++)
         {
           Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
-          majorColorList.Add(majorColors[i]);
-          minorColorList.Add(minorColors[i]);
+          _pairNumber.Add(i * 5 + j);
+          _majorColorList.Add(majorColors[i]);
+          _minorColorList.Add(minorColors[i]);
         }
       }
       return i * j;
     }
     static void Main(string[] args)
     {
-      int result = printColorMap();
+      int result = PrintColorMap();
       Debug.Assert(result == 25);
       Console.WriteLine("Count status - Success");
 
       //Check the sequence of major and minor colors
-      Debug.Assert(majorColorList[1] == "White");
-      Debug.Assert(minorColorList[1] == "Blue");
+      Debug.Assert(_majorColorList[1] == "White");
+      Debug.Assert(_minorColorList[1] == "Blue");
       Console.WriteLine("Sequence Check - Success");
 
-      Debug.Assert(majorColorList[10] == "Red");
-      Debug.Assert(minorColorList[10] == "Slate");
+      Debug.Assert(_majorColorList[10] == "Red");
+      Debug.Assert(_minorColorList[10] == "Slate");
       Console.WriteLine("Sequence Check - Success");
+
+      //Check the pair number
+      Debug.Assert(_pairNumber[10] == 11);
+      Console.WriteLine("Pair Number Check - Success");
+
       Console.WriteLine("All is well (maybe!)");
     }
   }
 }
+
