2085b6d1ce332a7861adcfd207c3c8b54e0d6fe9
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-04-26 14:54:54 +0530

    Update Misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,23 +1,46 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
-namespace MisalignedSpace {
-    class Misaligned {
-        static int printColorMap() {
-            string[] majorColors = {"White", "Red", "Black", "Yellow", "Violet"};
-            string[] minorColors = {"Blue", "Orange", "Green", "Brown", "Slate"};
-            int i = 0, j = 0;
-            for(i = 0; i < 5; i++) {
-                for(j = 0; j < 5; j++) {
-                    Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
-                }
-            }
-            return i * j;
+namespace MisalignedSpace
+{
+  class Misaligned
+  {
+    private static List<string> majorColorList = new List<string>();
+    private static List<string> minorColorList = new List<string>();
+
+    static int printColorMap()
+    {
+      string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+      string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
+      int i = 0, j = 0;
+      for (i = 0; i < 5; i++)
+      {
+        for (j = 0; j < 5; j++)
+        {
+          Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
+          majorColorList.Add(majorColors[i]);
+          minorColorList.Add(minorColors[i]);
         }
-        static void Main(string[] args) {
-            int result = printColorMap();
-            Debug.Assert(result == 25);
-            Console.WriteLine("All is well (maybe!)");
-        }
+      }
+      return i * j;
     }
+    static void Main(string[] args)
+    {
+      int result = printColorMap();
+      Debug.Assert(result == 25);
+      Console.WriteLine("Count status - Success");
+
+      //Check the sequence of major and minor colors
+      Debug.Assert(majorColorList[1] == "White");
+      Debug.Assert(minorColorList[1] == "Blue");
+      Console.WriteLine("Sequence Check - Success");
+
+      Debug.Assert(majorColorList[10] == "Red");
+      Debug.Assert(minorColorList[10] == "Slate");
+      Console.WriteLine("Sequence Check - Success");
+      Console.WriteLine("All is well (maybe!)");
+    }
+  }
 }
