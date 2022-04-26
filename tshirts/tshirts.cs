c8ca6a3a03febd9c31e3601510e0a89f1f1f570a
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-04-26 15:03:15 +0530

    Update tshirt.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -1,17 +1,25 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
-namespace TshirtSpace {
-    class Tshirt {
-        static string Size(int cms) {
-            if(cms < 38) {
-                return "S";
-            } else if(cms > 38 && cms < 42) {
-                return "M";
-            } else {
-                return "L";
-            }
-        }
+namespace TshirtSpace
+{
+    class Tshirt
+  {
+    static string Size(int cms)
+    {
+      if (cms < 38)
+      {
+        return "S";
+      }
+      else if (cms > 38 && cms < 42)
+      {
+        return "M";
+      }
+      else
+      {
+        return "L";
+      }
+    }
         static void Main(string[] args) {
             Debug.Assert(Size(37) == "S");
             Debug.Assert(Size(40) == "M");
