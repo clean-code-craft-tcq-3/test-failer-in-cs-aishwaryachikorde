e1f00983b8f44aaf66a5f9141bea3eead219a116
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-05-04 12:07:20 +0530

    Update misaligned.cs - Implementation

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -1,52 +1,36 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using misaligned;
 
 namespace MisalignedSpace
 {
-  class Misaligned
+  public class Misaligned
   {
-    private static List<string> _majorColorList = new List<string>();
-    private static List<string> _minorColorList = new List<string>();
-    private static List<int> _pairNumber = new List<int>();
-
-    static int PrintColorMap()
+    public static List<ColorCodeProperties> PrintColorMap()
     {
+      List<ColorCodeProperties> colorCodePairList = new List<ColorCodeProperties>();
       string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
       string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
-      int i = 0, j = 0;
+      
+
+      int i, j;
       for (i = 0; i < 5; i++)
       {
         for (j = 0; j < 5; j++)
         {
-          Console.WriteLine("{0} | {1} | {2}", i * 5 + j, majorColors[i], minorColors[i]);
-          _pairNumber.Add(i * 5 + j);
-          _majorColorList.Add(majorColors[i]);
-          _minorColorList.Add(minorColors[i]);
-        }
+          string colorMapToPrint = string.Format("{0, -8} | {1, -8} | {2, -8}", i * 5 + j + 1, majorColors[i], minorColors[j]);
+          colorCodePairList.Add(new ColorCodeProperties 
+            {
+              majorColor = majorColors[i], 
+              minorColor = minorColors[i], 
+              colorCodeToDisplay = colorMapToPrint
+            });
+
+          Console.WriteLine(colorMapToPrint);
+        } 
       }
-      return i * j;
-    }
-    static void Main(string[] args)
-    {
-      int result = PrintColorMap();
-      Debug.Assert(result == 25);
-      Console.WriteLine("Count status - Success");
-
-      //Check the sequence of major and minor colors
-      Debug.Assert(_majorColorList[1] == "White");
-      Debug.Assert(_minorColorList[1] == "Blue");
-      Console.WriteLine("Sequence Check - Success");
-
-      Debug.Assert(_majorColorList[10] == "Red");
-      Debug.Assert(_minorColorList[10] == "Slate");
-      Console.WriteLine("Sequence Check - Success");
-
-      //Check the pair number
-      Debug.Assert(_pairNumber[10] == 11);
-      Console.WriteLine("Pair Number Check - Success");
-
-      Console.WriteLine("All is well (maybe!)");
+      
+      return colorCodePairList;
     }
   }
 }
