a958a65c6774f68eb30fcd8b882af417b6507a8f
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-05-04 12:08:07 +0530

    Add files via upload

---
+++ misaligned/ColorCodeProperties.cs
@@ -0,0 +1,9 @@
+﻿namespace misaligned
+{
+  public class ColorCodeProperties
+  {
+    public string majorColor { get; set; } = string.Empty;
+    public string minorColor { get; set; } = string.Empty;
+    public string colorCodeToDisplay { get; set; } = string.Empty;
+  }
+}
---
+++ misaligned/MisalignedUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using MisalignedSpace;
+
+namespace misaligned
+{
+  class MisalignedUnitTest
+  {
+    static void Main(string[] args)
+    {
+      List<ColorCodeProperties> result = Misaligned.PrintColorMap();
+      Debug.Assert(result.Count == 25);
+      Debug.Assert(result[1].majorColor == "White");
+      Debug.Assert(result[1].minorColor == "Orange");
+      Debug.Assert(result[10].majorColor == "Black");
+      Debug.Assert(result[10].minorColor == "Blue");
+
+      //check if the alignment is correct
+      Debug.Assert(result[0].colorCodeToDisplay.Length == result[result.Count - 1].colorCodeToDisplay.Length);
+
+
+      Console.WriteLine("All is well (maybe!)");
+    }
+  }
+}
