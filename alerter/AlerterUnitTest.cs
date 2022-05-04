34a7ebfef12c30d8a042c1b9ab8a94672ff79ee2
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-05-04 15:04:34 +0530

    Alerter Test file

---
+++ alerter/AlerterUnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace alerter
+{
+  class AlerterUnitTest
+  {
+    static void Main(string[] args)
+    {
+      Alerter.AlertInCelsius(303.6f, "Production");
+      Debug.Assert(Alerter.alertFailureCount == 0);
+      
+      Alerter.AlertInCelsius(400.5f, "Test");
+      Debug.Assert(Alerter.alertFailureCount == 1);
+
+      Console.WriteLine("{0} alerts failed.", Alerter.alertFailureCount);
+      Console.WriteLine("All is well (maybe!)\n");
+    }
+  }
+}
