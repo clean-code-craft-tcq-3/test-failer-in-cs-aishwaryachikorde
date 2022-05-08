583b1e87cd05621cf0902b7c89f33952e0038a57
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-05-08 22:53:52 +0530

    AlerterUnitTest.cs-Values added for robust testing

--- alerter/AlerterUnitTest.cs
+++ alerter/AlerterUnitTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 
 namespace alerter
@@ -7,14 +7,21 @@ namespace alerter
   {
     static void Main(string[] args)
     {
-      Alerter.AlertInCelsius(303.6f, "Production");
-      Debug.Assert(Alerter.alertFailureCount == 0);
-      
-      Alerter.AlertInCelsius(400.5f, "Test");
-      Debug.Assert(Alerter.alertFailureCount == 1);
+      Alerter.AlertInCelsius(303.6f);
+      Debug.Assert(Alerter.AlertFailureCount == 0);
 
-      Console.WriteLine("{0} alerts failed.", Alerter.alertFailureCount);
+      Alerter.AlertInCelsius(-303.6f);
+      Debug.Assert(Alerter.AlertFailureCount == 0);
+
+      Alerter.AlertInCelsius(400.5f);
+      Debug.Assert(Alerter.AlertFailureCount == 1);
+
+      Alerter.AlertInCelsius(393f);
+      Debug.Assert(Alerter.AlertFailureCount == 2);
+
+      Console.WriteLine("{0} alerts failed.", Alerter.AlertFailureCount);
       Console.WriteLine("All is well (maybe!)\n");
     }
   }
 }
+
