b705e99c136994a19d3bf57d0e7f39c48c0ff88a
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-07-25 20:35:13 +0530

    Update MisalignedUnitTest.cs

--- misaligned/MisalignedUnitTest.cs
+++ misaligned/MisalignedUnitTest.cs
@@ -27,7 +27,7 @@ namespace misaligned
         Debug.Assert(ColorCode[i].PairNumber == i + 1);
         //Debug.Assert(ColorCode[0].MajorColor == "White");
         //Debug.Assert(ColorCode[0].MinorColor == "Blue");
-        //Debug.Assert(ColorCode[9].MajorColor == "Red");
+        Debug.Assert(ColorCode[10].MajorColor == "Red");
         //Debug.Assert(ColorCode[9].MinorColor == "Slate");
       }
 
