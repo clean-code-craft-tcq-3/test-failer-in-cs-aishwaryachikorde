cbab29291b150d10321998037f1100ccdffde94d
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-07-25 19:28:07 +0530

    Delete ColorCodeProperties.cs

--- misaligned/ColorCodeProperties.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace misaligned
-{
-  public class ColorCodeProperties
-  {
-    public string majorColor { get; set; } = string.Empty;
-    public string minorColor { get; set; } = string.Empty;
-    public string colorCodeToDisplay { get; set; } = string.Empty;
-  }
-}
