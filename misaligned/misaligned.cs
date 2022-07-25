88b575c8c47d0f9573b155983bcb514049372f44
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-07-25 19:30:02 +0530

    Create misaligned.cs

---
+++ misaligned/misaligned.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using misaligned;
+
+namespace MisalignedSpace
+{
+  public class Misaligned
+  {
+    static void Main()
+    {
+      string[] majorColors = { "White", "Red", "Black", "Yellow", "Violet" };
+      string[] minorColors = { "Blue", "Orange", "Green", "Brown", "Slate" };
+
+      List<ColorCodeProperties> colorCode = GetColorCodeMap(minorColors, majorColors);
+
+      PrintColorMapOnConsole(colorCode);
+
+      MisalignedUnitTest unitTest = new MisalignedUnitTest(colorCode);
+      unitTest.ColorCodeTest(minorColors, majorColors);
+
+      Console.WriteLine("All is well (maybe!)");
+    }
+
+    private static void PrintColorMapOnConsole(List<ColorCodeProperties> colorPair)
+    {
+      foreach (ColorCodeProperties colorCode in colorPair)
+      {
+        Console.WriteLine("{1, -8} | {2, -8} | {0, -8}", colorCode.MinorColor, colorCode.MajorColor, colorCode.PairNumber);
+      }
+    }
+
+    public static List<ColorCodeProperties> GetColorCodeMap(string[] minorColors, string[] majorColors)
+    {
+      List<ColorCodeProperties> colorCodePairList = new List<ColorCodeProperties>();
+      for (int i = 0; i < majorColors.Length; i++)
+      {
+        for (int j = 0; j < minorColors.Length; j++)
+        {
+          colorCodePairList.Add(new ColorCodeProperties
+          {
+            MinorColor = minorColors[j],
+            MajorColor = majorColors[i],
+            PairNumber = (i*5+j) +1
+
+          });
+        }
+      }
+
+      return colorCodePairList;
+    }
+  }
+}
+
+
