8e4672f40267a3b9c2ae4656eefb5942d439710c
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-07-25 19:37:46 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -7,11 +7,11 @@ namespace TshirtSpace
   {
     static string Size(int cms)
     {
-      if (cms <= 38)
+      if (cms < 38)
       {
         return "S";
       }
-      else if (cms > 38 && cms <= 42)
+      else if (cms >= 38 && cms <= 42)
       {
         return "M";
       }
